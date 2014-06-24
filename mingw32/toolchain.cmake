846e7f1b001bf5a670c3f7de0175aad70b7546b7
Motoko Kusanagi <kordex@mail.xnet.fi>
2014-06-24 20:32:28 +0300

    fixed

--- mingw32/toolchain.cmake
+++ mingw32/toolchain.cmake
@@ -10,10 +10,10 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
-set(OPENAL_INCLUDE_DIR /usr/share/tomcat/.jenkins/workspace/fi.xnet.dhewm3-mingw32-w64/mingw32/openal-soft-1.15.1-x86_64-w64-mingw32/include/)
-set(OPENAL_LIBRARY /usr/share/tomcat/.jenkins/workspace/fi.xnet.dhewm3-mingw32-w64/mingw32/openal-soft-1.15.1-x86_64-w64-mingw32/lib/libOpenAL32.dll.a)
+set(OPENAL_INCLUDE_DIR /usr/share/tomcat/.jenkins/workspace/fi.xnet.dhewm3-mingw32-w64mingw32/openal-soft-1.15.1-x86_64-w64-mingw32/include/)
+set(OPENAL_LIBRARY /usr/share/tomcat/.jenkins/workspace/fi.xnet.dhewm3-mingw32-w64mingw32/openal-soft-1.15.1-x86_64-w64-mingw32/lib/libOpenAL32.dll.a)
 
 set(SDL2_INCLUDE_DIR /usr/share/tomcat/.jenkins/workspace/fi.xnet.dhewm3-mingw32-w64/mingw32/SDL2-2.0.3-x86_64-w64-mingw32/include/)
 set(SDL2_LIBRARY /usr/share/tomcat/.jenkins/workspace/fi.xnet.dhewm3-mingw32-w64/mingw32/SDL2-2.0.3-x86_64-w64-mingw32/lib/libSDL2.a)
 set(SDL2DLL_LIBRARY /usr/share/tomcat/.jenkins/workspace/fi.xnet.dhewm3-mingw32-w64/mingw32/SDL2-2.0.3-x86_64-w64-mingw32/lib/libSDL2.dll.a)
-set(SDL2MAIN_LIBRARY /usr/share/tomcat/.jenkins/workspace/fi.xnet.dhewm3-mingw32-w64/mingw32/SDL2-2.0.3-x86_64-w64-mingw32/lib/libSDL2main.a)
+set(SDL2MAIN_LIBRARY -Wl,--whole-archive /usr/share/tomcat/.jenkins/workspace/fi.xnet.dhewm3-mingw32-w64/mingw32/SDL2-2.0.3-x86_64-w64-mingw32/lib/libSDL2main.a -Wl,--no-whole-archive)
